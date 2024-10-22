28fb3b94421ca41e157e3745ba256bbe1d7b9da8
lai2-24 <lai2-24@itismeucci.com>
2024-10-22 14:17:31 +0200

    ultimo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -17,13 +17,30 @@ public class Main {
         BufferedReader inTastiera = new BufferedReader(new InputStreamReader(System.in));
         String num;
         String messaggioRic;
+        String tentativiRichiesti;
         do {
             System.out.print("Inserisci il numero: ");
             num = inTastiera.readLine();
             out.writeBytes(num + "\n");
-            messaggioRic = 
-
+            messaggioRic = in.readLine();
+            if (messaggioRic.equals("!")) {
+                System.out.println("Numero errato o illeggibile");
+            } else if (messaggioRic.equals("<"))
+                System.out.println("Numero troppo piccolo");
+            else if (messaggioRic.equals(">")) {
+                System.out.println("Numero troppo grande");
+            } else {
+                tentativiRichiesti = in.readLine();
+                System.out.println("HAI INDOVINATO IN " + tentativiRichiesti + " tentativi");
+                System.out.println("Vuoi giocare un'altra partita? Scrivi s se desideri farne un'altra");
+                num = inTastiera.readLine();
+                out.writeBytes(num + "\n");
+                if (!num.equals("s")){   
+                   s.close();
+                   break;
+                }
+            }
         } while (true);
-        
+
     }
 }
